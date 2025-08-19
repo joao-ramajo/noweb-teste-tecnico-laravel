2197e57748d6c530d8aa546219280673f8be49e1
João Ramajo <joaoramajo744@gmail.com>
2025-08-18 23:21:00 -0300

    feat: implementa rota para logout e revogação do token de acesso

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -5,13 +5,14 @@ namespace App\Http\Controllers;
 use App\Http\Requests\AuthController\LoginRequest;
 use App\Models\User;
 use Illuminate\Database\Eloquent\ModelNotFoundException;
+use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Hash;
 use InvalidArgumentException;
 
 class AuthController extends Controller
 {
-    public function login(LoginRequest $request)
+    public function login(LoginRequest $request): JsonResponse
     {
         try{
             // busca usuario
@@ -35,5 +36,15 @@ class AuthController extends Controller
                     'message' => $e->getMessage()
                 ], 401);
         }
+    }
+
+    public function logout(Request $request): JsonResponse
+    {
+        $request->user()->currentAccessToken()->delete();
+
+        return response()
+            ->json([
+                'message' => 'Logou realizado com sucesso'
+            ], 200);
     }
 }
