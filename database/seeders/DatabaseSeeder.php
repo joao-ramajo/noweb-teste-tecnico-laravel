cd6b6944ca08601e1f987ccf98528eeec8559faa
João Ramajo <joaoramajo744@gmail.com>
2025-08-19 00:17:15 -0300

    feat: adiciona factory para noticias

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,9 +2,11 @@
 
 namespace Database\Seeders;
 
+use App\Models\Article;
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
 
 class DatabaseSeeder extends Seeder
 {
@@ -16,8 +18,11 @@ class DatabaseSeeder extends Seeder
         // User::factory(10)->create();
 
         User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
+            'name' => 'Adminstrador',
+            'email' => 'admin@gmail.com',
+            'password' => Hash::make('Aa123123')
         ]);
+
+        Article::factory(1)->create();
     }
 }
