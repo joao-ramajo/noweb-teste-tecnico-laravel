bf807014a0a4d636df02a3b54e16056c8931520a
João Ramajo <joaoramajo744@gmail.com>
2025-08-19 01:27:41 -0300

    chore: ajusta estrutura de `api.php`

--- routes/api.php
+++ routes/api.php
@@ -6,17 +6,17 @@ use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
 Route::post('/login', [AuthController::class, 'login'])->name('login');
-Route::post('/logout', [AuthController::class, 'logout'])->name('logout')->middleware(['auth:sanctum']);
 
-Route::prefix('users')->group(function() {
-    Route::middleware(['auth:sanctum'])->group(function() {
+Route::middleware(['auth:sanctum'])->group(function(){
+    Route::post('/logout', [AuthController::class, 'logout'])->name('logout')->middleware(['auth:sanctum']);
+
+    Route::prefix('users')->group(function() {
         Route::get('/', [UserController::class, 'index'])->name('users.index');
+        Route::post('/', [UserController::class, 'store'])->name('users.store');
     });
-    Route::post('/', [UserController::class, 'store'])->name('users.store');
+
+    Route::apiResource('articles', ArticleController::class);
 });
 
-Route::prefix('articles')->group(function() {
-    Route::get('/', [ArticleController::class, 'index'])->name('articles.index');
-});
 
-Route::apiResource('articles', ArticleController::class)->middleware(['auth:sanctum']);
+
