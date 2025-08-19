d5b4febf5f7de0ff752a0e5e939498cb69eabbd1
João Ramajo <joaoramajo744@gmail.com>
2025-08-19 02:21:35 -0300

    refactor: adiciona tratamento de erro e listagem dos registros alterados em `AlterArticlesName`

--- app/Console/Commands/AlterArticlesName.php
+++ app/Console/Commands/AlterArticlesName.php
@@ -3,6 +3,7 @@
 namespace App\Console\Commands;
 
 use App\Models\Article;
+use Exception;
 use Illuminate\Console\Command;
 use Illuminate\Support\Facades\DB;
 
@@ -27,24 +28,33 @@ class AlterArticlesName extends Command
      */
     public function handle()
     {
-        // Article::factory()->count(10)->create();
-        $articles_count = Article::all()->count();
-        $message = str_repeat('=', 40) . PHP_EOL;
-        $message .= "   ALTERANDO NOME DE $articles_count REGISTROS" . PHP_EOL;
-        $message .= str_repeat('=', 40) . PHP_EOL;
-        $this->info($message);
+        try{
+            Article::factory()->count(10)->create();
+            $articles_count = Article::all()->count();
 
-        $old_names = Article::pluck('title');
+            $message = str_repeat('=', 40) . PHP_EOL;
+            $message .= "   ALTERANDO NOME DE $articles_count REGISTROS" . PHP_EOL;
+            $message .= str_repeat('=', 40) . PHP_EOL;
+            $this->info($message);
 
-        DB::table('articles')->update([
-            'title' => 'noweb'
-        ]);
+            $old_names = Article::pluck('title');
 
-        $new_names = Article::pluck('title');
+            DB::table('articles')->update([
+                'title' => 'noweb'
+            ]);
 
-        $this->line('| Nome Antigo | Novo Nome |');
-        for($i = 0; $i < $articles_count; $i++){
-            $this->line("| {$old_names[$i]} | {$new_names[$i]}");
+            $new_names = Article::pluck('title');
+
+            $this->line('Nome Antigo | Novo Nome ');
+            for($i = 0; $i < $articles_count; $i++){
+                $this->line("{$old_names[$i]} -> {$new_names[$i]}");
+            }
+            $this->info(PHP_EOL);
+            $this->info('Registros alterados com sucesso !');
+            $this->line('Os registros gerados por este comando não se mantém no banco para evitar um acumulo de registros desnecessários');
+            DB::table('articles')->delete();
+        }catch(Exception $e){
+            $this->warn('Erro de execução: ' . $e->getMessage());
         }
     }
 }
--- database/migrations/2025_08_19_030325_create_articles_table.php
+++ database/migrations/2025_08_19_030325_create_articles_table.php
@@ -14,7 +14,7 @@ return new class extends Migration
         Schema::create('articles', function (Blueprint $table) {
             $table->id();
             $table->foreignId('user_id')->constrained()->onDelete('cascade');
-            $table->string('title', 50)->unique();
+            $table->string('title', 50);
             $table->text('content');
             $table->timestamps();
         });
