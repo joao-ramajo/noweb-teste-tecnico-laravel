07fa3c563a5a2318900d46505a093cfcafa6a2d6
João Ramajo <joaoramajo744@gmail.com>
2025-08-19 02:11:38 -0300

    feat: implementa alteração de nomes em massa em `AlterArticlesName`

--- app/Console/Commands/AlterArticlesName.php
+++ app/Console/Commands/AlterArticlesName.php
@@ -2,7 +2,9 @@
 
 namespace App\Console\Commands;
 
+use App\Models\Article;
 use Illuminate\Console\Command;
+use Illuminate\Support\Facades\DB;
 
 class AlterArticlesName extends Command
 {
@@ -25,9 +27,24 @@ class AlterArticlesName extends Command
      */
     public function handle()
     {
+        // Article::factory()->count(10)->create();
+        $articles_count = Article::all()->count();
         $message = str_repeat('=', 40) . PHP_EOL;
-        $message .= "   ALTERANDO NOME DE X REGISTROS" . PHP_EOL;
+        $message .= "   ALTERANDO NOME DE $articles_count REGISTROS" . PHP_EOL;
         $message .= str_repeat('=', 40) . PHP_EOL;
         $this->info($message);
+
+        $old_names = Article::pluck('title');
+
+        DB::table('articles')->update([
+            'title' => 'noweb'
+        ]);
+
+        $new_names = Article::pluck('title');
+
+        $this->line('| Nome Antigo | Novo Nome |');
+        for($i = 0; $i < $articles_count; $i++){
+            $this->line("| {$old_names[$i]} | {$new_names[$i]}");
+        }
     }
 }
