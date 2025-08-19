cf917012577056c4c873ba4d98dece97cd5bbcb2
João Ramajo <joaoramajo744@gmail.com>
2025-08-19 00:55:10 -0300

    tests: ajusta testes para criação de posts

--- tests/Feature/Http/Controllers/ArticleController/CreateArticleTest.php
+++ tests/Feature/Http/Controllers/ArticleController/CreateArticleTest.php
@@ -1,12 +1,14 @@
 <?php
 
 use App\Models\User;
+use Laravel\Sanctum\Sanctum;
 
 it('can create a article', function() {
     $user = User::factory()->create();
 
+    Sanctum::actingAs($user);
+
     $response = $this->postJson('/api/articles', [
-        'user_id' => $user->id,
         'title' => 'Last notice from the day',
         'content' => 'Lorem ipslum dolor amet'
     ]);
@@ -15,7 +17,18 @@ it('can create a article', function() {
         ->assertStatus(201);
 
     $this->assertDatabaseHas('articles', [
-        'title' => 'Last notice from the day'
+        'title' => 'Last notice from the day',
+        'user_id' => $user->id
     ]);
 
+});
+
+it('denies access from without token', function() {
+    $response = $this->postJson('/api/articles', [
+        'title' => 'Last notice from the day',
+        'content' => 'Lorem ipslum dolor amet'
+    ]);
+
+    $response
+        ->assertStatus(401);
 });
