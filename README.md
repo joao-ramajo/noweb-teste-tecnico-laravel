a33db15e996cf2a3ad347f0e2141f9c306f40ae2
João Ramajo <joaoramajo744@gmail.com>
2025-08-18 21:32:50 -0300

    feat: cria controller resource para usuarios

--- README.md
+++ README.md
@@ -2,3 +2,7 @@
 
 Este README tem como objetivo documentar o desenvolvimento de uma API como teste técnico para a vaga de desenvolvedor Backend na empresa NoWeb.
 
+## Tecnologias
+- Laravel 12
+- Sanctum
+- MySQL
--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,14 @@
 <?php
 
+use App\Http\Controllers\UserController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
+
+// Route::resource('/users', [UserController::class]);
+Route::prefix('users')->group(function() {
+    Route::get('/', [UserController::class, 'index'])->name('users.index');
+});
