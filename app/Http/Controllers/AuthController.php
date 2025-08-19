12e33b765665330e0592462bd075d9265d2ac998
João Ramajo <joaoramajo744@gmail.com>
2025-08-18 22:31:46 -0300

    feat: implementa rota de login básica

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -3,12 +3,36 @@
 namespace App\Http\Controllers;
 
 use App\Http\Requests\AuthController\LoginRequest;
+use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+use InvalidArgumentException;
 
 class AuthController extends Controller
 {
     public function login(LoginRequest $request)
     {
-        echo "realizando login";
+        try{
+            // busca usuario
+            $user = User::where('email', $request->input('email'))->firstOrFail();
+
+            // validar senha
+            if(!Hash::check($request->password, $user->password)){
+                throw new InvalidArgumentException('Email ou senha incorretos.');
+            }
+            // gerar token
+            $token = $user->createToken($user->email)->plainTextToken;
+
+            // retorna token
+            return response()
+                ->json([
+                    'token' => $token
+                ]);
+        }catch(InvalidArgumentException $e){
+            return response()
+                ->json([
+                    'message' => $e->getMessage()
+                ], 404);
+        }
     }
 }
